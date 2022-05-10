217d8d4908f1529eb757a1357b48af20ee9c54f4
martina <mazalazar@frba.utn.edu.ar>
2022-05-10 00:38:56 -0300

    KIS-21 leer del archivo e identificar operacion

--- consola/src/consola.c
+++ consola/src/consola.c
@@ -6,54 +6,101 @@
 #include "./commons/string.h"
 
 typedef struct {
-    char* parametros[2]; 
-    char* id; //NO_OP, I/O, READ, WRITE, COPY, EXIT
-} instruccion;
+	char* parametros[2];
+	char* id; //NO_OP, I/O, READ, WRITE, COPY, EXIT
+} Instruccion;
 
 // void exit_gracefully(int return_nr) {
 //   exit(return_nr);
 // }
 
+void definirCodigo(char* id, int* codigo) {
+	if (string_contains(id, "NO_OP")) {
+		*codigo = 1;
+	}
+	if (string_contains(id, "I/O")) {
+		*codigo = 2;
+	}
+	if (string_contains(id, "WRITE")) {
+		*codigo = 3;
+	}
+	if (string_contains(id, "COPY")) {
+		*codigo = 4;
+	}
+	if (string_contains(id, "READ")) {
+		*codigo = 5;
+	}
+	if (string_contains(id, "EXIT")) {
+		*codigo = 6;
+	}
+
+}
 
 int main(int argc, char** argv) {
 
 	//Conecta como cliente al Kernel
 	/*int sockfd = conectar_kernel();
-	saludo_inicial_kernel(sockfd);*/
+	 saludo_inicial_kernel(sockfd);*/
 
-	char* tamanio_proceso =  argv[2];
+	char* tamanio_proceso = argv[2];
 	char* filename = argv[1];
-	
 
-	FILE* input_file = fopen(filename, "r");
-    if (!input_file)
-        exit(EXIT_FAILURE);
+	FILE* input_file = fopen(
+			"/home/utnso/tp-2022-1c-9-12/consola/instrucciones.txt", "r");
+	//FILE* input_file = fopen("../Debug/instrucciones.txt", "r");
+	if (!input_file)
+		exit(EXIT_FAILURE);
 
-    char *contents = NULL;
-    size_t len = 0;
-    while (getline(&contents, &len, input_file) != -1){
+	char *contents = NULL;
+	size_t len = 0;
+	while (getline(&contents, &len, input_file) != -1) {
 		//contents = contenido de la linea
-		//WRITE 4 5
-		
-		char**  parametros = string_split(contents, " ");
 
-		char* id[10] = parametros[0];
-		char* parametro1[10] = parametros[1];
-		char* parametro2[10] = parametros[2];
+		char** parametros = string_split(contents, " ");
 
-        printf("%s", id);
-		printf("%s", id);
+		char* id = parametros[0];
+		char* parametro1 = parametros[1];
+		char* parametro2 = parametros[2];
 
-    }
+		printf("\n%s", id);
 
-    fclose(input_file);
-    free(contents);
+		int codigo = -1;
+
+		definirCodigo(id, &codigo);
+
+		switch (codigo) {
+		case 1:
+			printf("\n%s", parametro1);
+			break;
+		case 2:
+			printf("\n%s", parametro1);
+			break;
+		case 3:
+			printf("\n%s", parametro1);
+			printf("\n%s", parametro2);
+			break;
+		case 4:
+			printf("\n%s", parametro1);
+			printf("\n%s", parametro2);
+			break;
+		case 5:
+			printf("\n%s", parametro1);
+			break;
+		default:
+			printf("\n No hay parametros para esta operacion");
+			break;
+		}
+
+		printf("\n codigo de op %d", codigo);
+	}
+
+	fclose(input_file);
+	free(contents);
 
 	//leerInstrucciones(argv[0]);
 	//close(sockfd);
 	return EXIT_SUCCESS;
 }
-
 
 //printf("%s",argv[1]); comprobar que el path es el que se mando 
 //"../intrucciones.txt"
