a03391f599a6859380c7f7e0e89a9497a1b52958
martina <mazalazar@frba.utn.edu.ar>
2022-05-14 02:04:31 -0300

    agrego instrucciones a la lista

--- consola/src/consola.c
+++ consola/src/consola.c
@@ -2,66 +2,96 @@
 #include <stdlib.h>
 // #include "cliente.h"
 #include <sys/stat.h>
-#include "./consolaConfig.h"
-#include "./commons/string.h"
-#include "./commons/collections/queue.h"
+#include <commons/log.h>
+#include <commons/config.h>
+#include <commons/string.h>
+#include <commons/collections/list.h>
 
-typedef struct {
-	char* parametros[2];
-	int id; //NO_OP, I/O, READ, WRITE, COPY, EXIT
-} Instruccion;
 
 // void exit_gracefully(int return_nr) {
 //   exit(return_nr);
 // }
 
-void definirCodigo(char* id, int* codigo) {
+//------------------------------------------Estructuras ----------------------------------------------------------
+typedef struct {
+	t_list* parametros;
+	int id; //NO_OP, I/O, READ, WRITE, COPY, EXIT
+} Instruccion;
+
+//typedef struct {
+//	uint32_t size;
+//	void* stream;
+//} t_buffer;
+
+//typedef struct {
+//	uint8_t codigo_operacion;
+//	t_buffer* buffer;
+//} t_paquete;
+
+//----------------------------------Funciones---------------------------------------------
+int definirCodigo(char* id) {
 	if (string_contains(id, "NO_OP")) {
-		*codigo = 1;
+		return 1;
 	}
 	if (string_contains(id, "I/O")) {
-		*codigo = 2;
+		return 2;
 	}
 	if (string_contains(id, "WRITE")) {
-		*codigo = 3;
+		return 3;
 	}
 	if (string_contains(id, "COPY")) {
-		*codigo = 4;
+		return 4;
 	}
 	if (string_contains(id, "READ")) {
-		*codigo = 5;
+		return 5;
 	}
 	if (string_contains(id, "EXIT")) {
-		*codigo = 6;
+		return 6;
+	}else{
+		return -1;
 	}
 }
 
-int definirCantidadParametros(codigo){
-	switch (codigo) {
-		case 1:
-			return 1;
+int getCantidadParametros(int id) {
+	switch (id) {
+	case 1: case 2: case 5:
+		return 1;
 		break;
-		case 2:
-			return 1;
+	case 3: case 4:
+		return 2;
 		break;
-		case 3:
-			return 2;
+	case 6:
+		return 0;
 		break;
-		case 4:
-			return 2;
-		break;
-		case 5:
-			return 1;
-		break;
-		case 5:
-			return 0;
-		break;
-		default:
-		printf("\n No hay parametros para esta operacion");
+	default:
+		printf("\n Se ingreso una operacion incorrecta");
+		return -1;
 		break;
 	}
 }
 
+void mostrar_instrucciones(t_list* list){
+	int aux = list_size(list);
+	Instruccion* instruccion = malloc(sizeof(instruccion));
+
+	for(int i=0;i<aux;i++){
+		instruccion= list_get(list,i);
+		printf("El codigo de la instruccion es: %d ", instruccion->id);
+		mostrar(instruccion->parametros);
+	}
+}
+
+void mostrar_parametros(t_list* list){
+	int aux = list_size(list);
+
+	for(int i=0;i<aux;i++){
+		int* parametro= list_get(list,i);
+		printf("%d ", *parametro);
+	}
+	printf("\n");
+}
+
+//_-----------------------------------Funcionalidad------------------------------------------
 
 int main(int argc, char** argv) {
 
@@ -72,7 +102,7 @@ int main(int argc, char** argv) {
 	char* tamanio_proceso = argv[2];
 	char* filename = argv[1];
 
-	FILE* input_file = fopen("/home/utnso/tp-2022-1c-9-12/consola/instrucciones.txt", "r");//harcodeado
+	FILE* input_file = fopen("/home/utnso/tp-2022-1c-9-12/consola/instrucciones.txt", "r"); //harcodeado
 
 	if (!input_file)
 		exit(EXIT_FAILURE);
@@ -80,26 +110,28 @@ int main(int argc, char** argv) {
 	char *contents = NULL;
 	size_t len = 0;
 
-	t_queue * instrucciones = queue_create();
+	t_list* instrucciones = list_create();
 
-	while (getline(&contents, &len, input_file) != -1) {//contents = contenido de la linea
+	while (getline(&contents, &len, input_file) != -1) { //contents = contenido de la linea
 
+		char** linea = string_split(contents, " ");
+		int nombre = linea[0];
+		int id = definirCodigo(nombre);
 
-		char** parametros = string_split(contents, " ");
+		Instruccion* instruccion = malloc(sizeof(Instruccion));
+		instruccion->id = id;
+		instruccion->parametros = list_create();
 
-		char* id = parametros[0];
-		char* parametro1 = atoi(parametros[1]);
-		char* parametro2 = atoi(parametros[2]); //dudoso para saber si te va a venir null o no
+		for (int i = 1;getCantidadParametros(id); i++) {
+			int* parametro = malloc(sizeof(int));
+			*parametro = atoi(linea[i]);
+			list_add(instruccion->parametros, parametro);
+		}
+		list_add(instrucciones, instruccion);
 
-		printf("\n%s", id);
-
-		int codigo = -1;
-
-		definirCodigo(id, &codigo);
-
-
-		printf("\n codigo de op %d", codigo);
 	}
+
+	mostrar_instrucciones(instrucciones);
 
 	fclose(input_file);
 	free(contents);
