3edf9861f9e76fc810c707e457b7324d54d9c94d
alanrysz <alandvc@gmail.com>
2022-07-13 01:13:40 -0300

    se modifica una función

--- Memoria/src/swap.h
+++ Memoria/src/swap.h
@@ -42,7 +42,7 @@
 	//SWAP
 	void iniciarSwap();
 	int existe_archivo(char* path);
-	void crear_archivo_swap(int pid);
+	void crear_archivo_swap(int pid, int tamanioProceso);
 	void eliminar_archivo_swap(int pid);
 	char* obtener_nombre_archivo(int pid);
 	void guardar_archivo_en_swap(int pid, int tamanoProceso);
