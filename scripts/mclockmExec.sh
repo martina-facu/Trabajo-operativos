3bfa29190b2a6edf8d8fef0293be7b8132984330
Hernan Rita <hernanrita@gmail.com>
2022-07-15 23:03:01 -0300

    Se finalizan los scripts de entrega. Resta probarlos

--- scripts/mclockmExec.sh
+++ scripts/mclockmExec.sh
@@ -10,10 +10,10 @@ export CONFPATH=$WORKSPACEPATH/confPruebas
 export LD_LIBRARY_PATH=$WORKSPACEPATH/Shared
 
 #	Copio los archivos de configuracion BASE
-cp $CONFPATH/memoria.config.base $MEMORYPATH/memoria.config
-cp $CONFPATH/cpu.config.base $CPUPATH/cpu.config
-cp $CONFPATH/kernel.config.base $KERNELPATH/kernel.config
-cp $CONFPATH/consola.config.base $CONSOLEPATH/consola.config
+cp $CONFPATH/memoria.config.mclockm $MEMORYPATH/memoria.config
+cp $CONFPATH/cpu.config.mclockm $CPUPATH/cpu.config
+cp $CONFPATH/kernel.config.mclockm $KERNELPATH/kernel.config
+cp $CONFPATH/consola.config.mclockm $CONSOLEPATH/consola.config
 
 #	Ejecuto primero memoria
 cd $MEMORYPATH
@@ -32,11 +32,5 @@ $KERNELPATH/Kernel &
 #	Levanto la primera consola
 cd $CONSOLEPATH
 sleep 1
-($CONSOLEPATH/Consola $CONFPATH/BASE_1 1024)  &
-
-#	Levanto la segunda consola
-($CONSOLEPATH/Consola $CONFPATH/BASE_2 1024)  &
-
-#	Levanto la tercera consola
-($CONSOLEPATH/Consola $CONFPATH/BASE_2 1024)  &
+($CONSOLEPATH/Consola $CONFPATH/MEMORIA_1  4096)  &
 
