4e1941f7521af0c4dd8071a330a44c754a196e64
alanrysz <alandvc@gmail.com>
2022-07-19 01:23:07 -0300

    se crea el archivo funciones_cpu

---
+++ Memoria/src/funciones_cpu.h
@@ -0,0 +1,17 @@
+/*
+ * funciones_cpu.h
+ *
+ *  Created on: 18 jul. 2022
+ *      Author: utnso
+ */
+
+#include "main.h"
+
+#ifndef FUNCIONES_CPU_H_
+#define FUNCIONES_CPU_H_
+
+
+void* funciones_cpu();
+
+
+#endif /* FUNCIONES_CPU_H_ */
